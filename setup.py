80463fe95217d19995e4ca32e42d8183c9f5a51a
superorakul <buslaev81@gmail.com>
2018-08-05 22:39:28 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The head first',
+    author='Dystopia',
+    author_email='nn@nn.com',
+    py_modules=['vsearch'],
+)
